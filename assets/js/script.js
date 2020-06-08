8b4137e12fc047b6a67ca998b61b846006f93d35
seandaviesjpw@gmail.com <seandaviesjpw@gmail.com>
2020-06-08 23:05:56 +0100

    Style changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,9 +77,11 @@ function openPage(url) {
   if(timer != null) {
     clearTimeout(timer);
   }
+
   if(url.indexOf("?") == -1){
     url = url + "?";
   }
+  
   var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
   $("#mainContent").load(encodedUrl);
   $("body").scrollTop(0);
