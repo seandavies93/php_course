b901bae1d60a57d688b13d7ca84edfde7710ff4f
seandaviesjpw@gmail.com <seandaviesjpw@gmail.com>
2020-06-09 23:28:13 +0100

    Minor alteration

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -12,9 +12,11 @@
     public function login($un, $pw) {
       $pw = md5($pw);
       $query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND password='$pw'");
+
       if(mysqli_num_rows($query) == 1) {
         return true;
       }
+
       array_push($this->errorArray, Constants::$loginFailed);
       return false;
     }
@@ -25,6 +27,7 @@
       $this->validateLastName($ln);
       $this->validateEmails($em, $em1);
       $this->validatePasswords($pw, $pw1);
+
       if(empty($this->errorArray)) {
         //insert into database
         return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
@@ -34,9 +37,11 @@
     }
 
     public function getError($error) {
+
       if(!in_array($error, $this->errorArray)) {
         $error = "";
       }
+
       return "<span class='errorMessage'>$error</span>";
     }
 
@@ -50,11 +55,14 @@
     }
 
     private function validateUsername($un) {
+
       if(strlen($un) > 25 || strlen($un) < 5) {
         array_push($this->errorArray, "Your username must be between 5 and 25 characters long");
         return;
       }
+
       $checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
+
       if(mysqli_num_rows($checkUsernameQuery) != 0) {
         array_push($this->errorArray, Constants::$usernameTaken);
         return;
@@ -62,6 +70,7 @@
     }
 
     private function validateFirstName($fn) {
+
       if(strlen($fn) > 25 || strlen($fn) < 2) {
         array_push($this->errorArray, Constants::$firstNameLength);
         return;
@@ -69,6 +78,7 @@
     }
 
     private function validateLastName($ln) {
+
       if(strlen($ln) > 25 || strlen($ln) < 2) {
         array_push($this->errorArray, Constants::$lastNameLength);
         return;
@@ -76,14 +86,17 @@
     }
 
     private function validateEmails($em, $em1) {
+
       if($em != $em1) {
         array_push($this->errorArray, Constants::$emailsDoNotMatch);
         return;
       }
+
       if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
         array_push($this->errorArray, Constants::$invalidEmails);
         return;
       }
+
       $checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$em'");
       if(mysqli_num_rows($checkEmailQuery) != 0) {
         array_push($this->errorArray, Constants:: $emailTaken);
@@ -92,14 +105,17 @@
     }
 
     private function validatePasswords($pw, $pw1) {
+      
       if($pw != $pw1) {
         array_push($this->errorArray, Constants::$passwordsDoNotMatch);
         return;
       }
+
       if(preg_match('/[^A-Za-z0-9]/', $pw)) {
         array_push($this->errorArray, Constants::$invalidPassword);
         return;
       }
+
       if(strlen($pw) > 30 || strlen($pw) < 8) {
         array_push($this->errorArray, Constants::$passwordsWrongLength);
         return;
